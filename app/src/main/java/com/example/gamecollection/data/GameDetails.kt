10ce45cd6c6c574af83d187db26c5c2ebb07f8b0
Steven Bui <bui.stvn@gmail.com>
2022-03-11 00:54:47 -0800

    Create game related data classes

---
+++ app/src/main/java/com/example/gamecollection/data/GameDetails.kt
@@ -0,0 +1,41 @@
+package com.example.gamecollection.data
+
+data class GameDetails(
+    val id: Int,
+    val name: String,
+    val description_raw: String,
+    val released: String,
+    val background_image: String,
+    val rating: Int,
+    val stores: List<Store>,
+    val developers: List<Developer>,
+    val genres: List<Genre>,
+    val tags: List<Tag>,
+    val esrb_rating: ESRBRating
+)
+
+data class Store(
+    val store: StoreDetails
+)
+
+data class StoreDetails(
+    val name: String,
+    val domain: String
+)
+
+data class Developer(
+    val id: Int,
+    val name: String
+)
+
+data class Genre(
+    val name: String
+)
+
+data class Tag(
+    val name: String
+)
+
+data class ESRBRating(
+    val name: String
+)
---
+++ app/src/main/java/com/example/gamecollection/data/GameListItem.kt
@@ -0,0 +1,8 @@
+package com.example.gamecollection.data
+
+data class GameListItem(
+    val id: Int,
+    val name: String,
+    val released: String,
+    val background_image: String
+)
