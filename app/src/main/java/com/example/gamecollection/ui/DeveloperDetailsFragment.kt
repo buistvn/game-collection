579e5727bea1be39f966282221cf8ebfe69830bd
Steven Bui <bui.stvn@gmail.com>
2022-03-17 17:42:49 -0700

    Convert DeveloperDetailsActivity to fragment

--- app/src/main/java/com/example/gamecollection/ui/DeveloperDetailsFragment.kt
+++ app/src/main/java/com/example/gamecollection/ui/DeveloperDetailsFragment.kt
@@ -1,80 +1,78 @@
 package com.example.gamecollection.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.core.text.HtmlCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.gamecollection.R
-import com.example.gamecollection.api.RAWGService
 import com.example.gamecollection.data.GameListItem
-import com.example.gamecollection.data.GameRepository
 import com.example.gamecollection.data.LoadingStatus
 import com.google.android.material.progressindicator.CircularProgressIndicator
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
 const val EXTRA_DEVELOPER_ID = "com.example.gamecollection.DEVELOPER_ID"
 
-class DeveloperDetailsActivity : AppCompatActivity() {
-    private val tag = "DeveloperDetailActivity"
+class DeveloperDetailsFragment : Fragment(R.layout.developer_details) {
+    private val TAG = "DeveloperDetailsFragment"
+    private var developerID: Int? = null
+
     private val gameSearchViewModel: GameSearchViewModel by viewModels()
     private val developerViewModel: DeveloperDetailsViewModel by viewModels()
+
     private lateinit var gameListAdapter: GameListAdapter
-    private var developerID: Int? = null
 
     private lateinit var searchResultListRV: RecyclerView
     private lateinit var loadingIndicator: CircularProgressIndicator
     private lateinit var searchErrorTV: TextView
     private lateinit var holder: LinearLayout
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_developer_details)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        loadingIndicator = findViewById(R.id.developer_loading_indicator)
-        searchResultListRV = findViewById(R.id.developer_RV)
+        loadingIndicator = view.findViewById(R.id.developer_loading_indicator)
+        searchResultListRV = view.findViewById(R.id.developer_RV)
+        searchErrorTV = view.findViewById(R.id.developer_search_error)
+        holder = view.findViewById(R.id.developer_view)
+
         gameListAdapter = GameListAdapter(::onGameListItemClick)
-        searchErrorTV = findViewById(R.id.developer_search_error)
-        holder = findViewById(R.id.developer_view)
 
-        searchResultListRV.layoutManager = GridLayoutManager(this,2)
+        searchResultListRV.layoutManager = GridLayoutManager(requireContext(), 2)
         searchResultListRV.setHasFixedSize(true)
         searchResultListRV.adapter = gameListAdapter
 
-        gameSearchViewModel.results.observe(this) { results ->
+        gameSearchViewModel.results.observe(viewLifecycleOwner) { results ->
             gameListAdapter.updateGameListItems(results?.results)
         }
 
+        /*
         if (intent != null && intent.hasExtra(EXTRA_DEVELOPER_ID)) {
             developerID = intent.getSerializableExtra(EXTRA_DEVELOPER_ID) as Int
             Log.d(tag, developerID!!.toString())
             developerViewModel.loadResults(developerID!!, RAWG_API_KEY)
         }
+        */
 
-        developerViewModel.results.observe(this) { results ->
+        developerViewModel.results.observe(viewLifecycleOwner) { results ->
             if (results != null) {
                 val games = results.games
-                findViewById<TextView>(R.id.Developer_Name).text = results.name
-                findViewById<TextView>(R.id.developer_games).text = "Total Games by this Developer: \t#"+ games.toString()
-
+                view.findViewById<TextView>(R.id.Developer_Name).text = results.name
+                view.findViewById<TextView>(R.id.developer_games).text = "Total Games by this Developer: \t#"+ games.toString()
 
                 Glide.with(this)
                     .load(results.background_image)
-                    .into(findViewById(R.id.dev_image))
+                    .into(view.findViewById(R.id.dev_image))
 
-                if(results.description.isNullOrEmpty()){
-                    findViewById<TextView>(R.id.Developer_Details).text = ""
-                    findViewById<TextView>(R.id.Developer_Details).visibility = View.INVISIBLE
-                }else {
-                    val desc: TextView = findViewById(R.id.Developer_Details)
+                if (results.description.isNullOrEmpty()) {
+                    view.findViewById<TextView>(R.id.Developer_Details).text = ""
+                    view.findViewById<TextView>(R.id.Developer_Details).visibility = View.INVISIBLE
+                }
+                else {
+                    val desc: TextView = view.findViewById(R.id.Developer_Details)
                     val long = HtmlCompat.fromHtml(results.description, HtmlCompat.FROM_HTML_MODE_COMPACT)
                     val size = if (long.length < 200) long.length else 200
                     val temp = long.chunked(size)[0].split(".").toMutableList()
@@ -84,26 +82,51 @@ class DeveloperDetailsActivity : AppCompatActivity() {
                     desc.setOnClickListener {
                         if (desc.text == short) {
                             desc.text = long
-                        } else {
+                        }
+                        else {
                             desc.text = short
                         }
                     }
                 }
 
-
-                if (games > 20){
-                    gameSearchViewModel.loadResults(RAWG_API_KEY, null, null, null, "20", null, developerID.toString())
-                }else if (games <= 2 || games %2 == 0){
-                    gameSearchViewModel.loadResults(RAWG_API_KEY, null, null, null, games.toString(), null, developerID.toString())
+                if (games > 20) {
+                    gameSearchViewModel.loadResults(
+                        RAWG_API_KEY,
+                        null,
+                        null,
+                        null,
+                        "20",
+                        null,
+                        developerID.toString()
+                    )
                 }
-                else{
-                    gameSearchViewModel.loadResults(RAWG_API_KEY, null, null, null, (games - 1).toString(), null, developerID.toString())
+                else if (games <= 2 || games %2 == 0) {
+                    gameSearchViewModel.loadResults(
+                        RAWG_API_KEY,
+                        null,
+                        null,
+                        null,
+                        games.toString(),
+                        null,
+                        developerID.toString()
+                    )
+                }
+                else {
+                    gameSearchViewModel.loadResults(
+                        RAWG_API_KEY,
+                        null,
+                        null,
+                        null,
+                        (games - 1).toString(),
+                        null,
+                        developerID.toString()
+                    )
                 }
             }
         }
 
-        gameSearchViewModel.loading.observe(this){ loading->
-            when(loading){
+        gameSearchViewModel.loading.observe(viewLifecycleOwner) { loading->
+            when (loading) {
                 LoadingStatus.LOADING -> {
                     loadingIndicator.visibility = View.VISIBLE
                     holder.visibility = View.INVISIBLE
@@ -124,10 +147,6 @@ class DeveloperDetailsActivity : AppCompatActivity() {
     }
 
     private fun onGameListItemClick(gameListItem: GameListItem) {
-        Log.d(tag, gameListItem.toString())
-        val intent = Intent(this, GameDetailActivity::class.java).apply {
-            putExtra(EXTRA_GAME_LIST_ITEM, gameListItem)
-        }
-        startActivity(intent)
+
     }
 }
